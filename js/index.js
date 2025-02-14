af4cc8e0204aa8ff0ef182216d3b1441e0d7f4a6
Trent <hlsagabaen@gmail.com>
2025-02-15 02:25:23 +0800

    minor changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector(".title");
-const text = "Surprise! Open it pls".split("");
+const text = "Surprise! Open it".split("");
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== " ") {
     title.innerHTML += `<span>${text[index]}<span/>`;
